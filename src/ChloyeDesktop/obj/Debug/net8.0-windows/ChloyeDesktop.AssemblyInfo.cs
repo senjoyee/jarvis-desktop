694f4093d15cd781189b1add44c2e30ee425bf39
Joyee Sen <sen.joyee@gmail.com>
2026-02-09 19:03:35 +0000

    style: update font to Geist Sans throughout app and unify message fonts to 15px

--- src/ChloyeDesktop/obj/Debug/net8.0-windows/ChloyeDesktop.AssemblyInfo.cs
+++ src/ChloyeDesktop/obj/Debug/net8.0-windows/ChloyeDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0f7d5d9ec34156d0844900968cb3d478043dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef7d1783a23d15d613fc3788e86914cfc4e7696")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
