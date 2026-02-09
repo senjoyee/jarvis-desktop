0f0f7d5d9ec34156d0844900968cb3d478043dc8
Joyee Sen <sen.joyee@gmail.com>
2026-02-09 17:25:09 +0000

    style: unify background colors to #212121 and differentiate input box with #2d2d2d

--- src/ChloyeDesktop/obj/Debug/net8.0-windows/ChloyeDesktop.AssemblyInfo.cs
+++ src/ChloyeDesktop/obj/Debug/net8.0-windows/ChloyeDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1d57d66499e2376d4216cd6f0e34771208c743")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4106733e2375855099b1d5cc1a36c43836280c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChloyeDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
